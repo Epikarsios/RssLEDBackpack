507735bfab3ec4b5ee60f4f96e07cb401573b70e
Epikarsios <lucas035@cougar.csusm.edu>
2017-08-04 23:03:10 +0000

    Added opening to start of RssFeed

--- RssFeed.py
+++ RssFeed.py
@@ -4,6 +4,19 @@ import time
 #create feed caled Rss
 Rss = feedparser.parse('http://feeds.reuters.com/Reuters/domesticNews')
 
+# Prints News + Logo
+
+print ("Here Comes The News ")
+print ("  *  ")
+time.sleep(.2)
+print (" * * ")
+time.sleep(.2)
+print ("*   *")
+time.sleep(.2)
+print (" * * ")
+time.sleep(.2)
+print ("  * ")
+print ("**************************************************************")
 #Loop to iterate through all titles in feed sleeping for 1 second between printing
 
 for i in Rss.entries:
