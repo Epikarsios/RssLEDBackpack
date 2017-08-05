e18acab3369c5d7f6b3e1b342b435c2527bd1bda
Epikarsios <lucas035@cougars.csusm.edu>
2017-08-04 21:39:33 -0700

    added .sh to install needed libraries

---
+++ RssLEDBackpack.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# 
+# RssLEDBackpack
+# ========
+# A RssLED installer for raspberry pi users.
+# 
+# https://github.com/Epikarsios/RssLEDBackpack
+# 
+# All code is copyright (c) Epikarsios 2017
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# 
+# 
+
+echo "\n"
+echo "Updating all existing packages...\n"
+sudo apt-get -y update
+
+echo "\n"
+echo "Getting requierd libraries \n"
+ 
+
+
+echo "\n"
+echo "Installing Python Libraries"
+sudo apt-get install -y build-essential python-dev python-smbus python-pip  python-imaging git
+echo "Installing Adafruit LEDBackpack Libraries\n"
+git clone https://github.com/adafruit/Adafruit_Python_LED_Backpack.git
+cd Adafruit_Python_LED_Backpack
+echo  "Running Adafruit Install \n"
+sudo python setup.py install 
+echo "Getting Feedparser \n"
+sudo pip install feedparser
+ 
+echo "\n"
+echo "Done! Read the readme file to set up a project\n"
+exit 0
